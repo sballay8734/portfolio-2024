941db0a5a0fe62985a8c9b03e65444195bc2cb8a
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2024-05-25 09:34:29 -0400

    config: daisyUI and tailwind install

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
+
+// !TODO: WAVE, ANDI and AXE-CORE web browser extensions for accessibility
+
+// TODO: Theme change for daisyUI
+// https://github.com/saadeghi/theme-change
